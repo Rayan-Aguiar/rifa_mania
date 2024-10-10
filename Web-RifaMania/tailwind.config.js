8bd86aba6ed6bb1a70dd01642fc44a2e62da4089
rayan-aguiar <rayansiqueira@gmail.com>
2024-10-10 12:49:24 -0300

    chore: Set colors system

--- Web-RifaMania/tailwind.config.js
+++ Web-RifaMania/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0C756F',   
+        secondary: '#FDCF6F',  
+        whiteCustom: '#EFEADB', 
+        blackCustom: '#000201', 
+      }
+    },
   },
   plugins: [],
 }
