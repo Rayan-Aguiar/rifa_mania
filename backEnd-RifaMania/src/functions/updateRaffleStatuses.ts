d06f18475bfd60a2b51c816d956c81c55f26560e
rayan-aguiar <rayansiqueira@gmail.com>
2025-02-26 11:56:40 -0300

    fix: Corrigido a função do cronJob
    - não estava considerando que rifas já concluidas e estava voltando para sortear

--- backEnd-RifaMania/src/functions/updateRaffleStatuses.ts
+++ backEnd-RifaMania/src/functions/updateRaffleStatuses.ts
@@ -1,42 +1,44 @@
 import { RaffleStatus } from "../constants/raffleStatus";
 import { prisma } from "../lib/prisma";
 
+export async function updateRaffleStatuses() {
+  const now = new Date();
 
-export async function updateRaffleStatuses(){
-    const now = new Date();
+  await prisma.raffle.updateMany({
+    where: {
+      drawDate: {
+        lte: now,
+      },
+      status: {
+        not: RaffleStatus.SORTING,
+      },
+    },
+    data: {
+      status: RaffleStatus.SORTING,
+      closed: true,
+    },
+  });
 
-    await prisma.raffle.updateMany({
-        where: {
-            drawDate:{
-                lte: now
-            },
-            status:{
-                not: RaffleStatus.SORTING
-            },
-        },
-        data:{
-            status: RaffleStatus.SORTING,
-            closed: true
-        }
-    })
+  const rafflesToUpdate = await prisma.raffle.findMany({
+    where: {
+      closed: false,
+      availableNumbersCount: 0,
+      AND: [
+        { status: RaffleStatus.ONLINE },
+        { status: { not: RaffleStatus.CONCLUDED } },
+      ],
+    },
+  });
 
-    const rafflesToUpdate = await prisma.raffle.findMany({
-        where:{
-            closed: false,
-            status: RaffleStatus.ONLINE,
-            availableNumbersCount: 0
-        }
-    })
-
-    await prisma.raffle.updateMany({
-        where:{
-            id:{
-                in: rafflesToUpdate.map(r => r.id)
-            },
-        },
-        data:{
-            status: RaffleStatus.SORTING,
-            closed: true
-        }
-    })
+  await prisma.raffle.updateMany({
+    where: {
+      id: {
+        in: rafflesToUpdate.map((r) => r.id),
+      },
+    },
+    data: {
+      status: RaffleStatus.SORTING,
+      closed: true,
+    },
+  });
 }
